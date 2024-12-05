f00a919d753d336375a29c0cd811c4e40903854e
SomaTakata <somatakata@gmail.com>
2024-12-05 16:33:19 +0900

    ページのレイアウトを更新し、最小高さを設定して中央揃えに変更

--- src/app/2/page.tsx
+++ src/app/2/page.tsx
@@ -1,7 +1,7 @@
 export default function PersonalityTest() {
   return (
-    <div className="h-full justify-center bg-white flex flex-col items-center px-5 py-12  gap-20">
-      <div className="relative animate-ping h-14 w-14 bg-blue-600 rounded-full"></div>
+    <div className="h-screen  bg-white flex flex-col items-center justify-center px-5 py-12 gap-20">
+      <div className="animate-ping h-14 w-14 bg-blue-600 rounded-full"></div>
       <div className="text-slate-700 text-center flex flex-col gap-2 mb-12">
         <p className="text-3xl font-bold">当てはまるものを</p>
         <p className="text-3xl font-bold">教えてください</p>
